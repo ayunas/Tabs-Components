e64f11d58d1bbd2bab4674797118e36908ef59ad
ayunas <ayunas@gmail.com>
2019-03-23 19:44:04 -0500

    completed the Tabs Component by creating a JS object with a select method.  The event listener is on the Tab component, and when clicked, the select method removes the selected class from all the tabs and items, and then puts the selected class on the tab and item created in the JS object.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -10,13 +10,22 @@
 class Tab {
     constructor(tab) {
         this.tab = tab;
-        
+        this.data = tab.dataset.tab;
+        this.item = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
+        this.tab.addEventListener('click', () => this.select());
     }
     
+    select() {
+        console.log('i have been clicked!');
+        tabs.forEach( tab => tab.classList.remove('tabs-link-selected'));
+        items.forEach( item => item.classList.remove('tabs-item-selected'));
+        
+        this.tab.classList.add('tabs-link-selected');
+        this.item.classList.add('tabs-item-selected');
+    }
 }
-
-
 const tabs = document.querySelectorAll('.tabs-link');
+const items = document.querySelectorAll('.tabs-item');
 
 tabs.forEach(tab => new Tab(tab));
 
